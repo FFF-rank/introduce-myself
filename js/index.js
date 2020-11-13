2e381527accf0c18980e60dbae79528c17917c87
FFF-rank <529012379@qq.com>
2020-11-13 12:28:20 +0800

    4.8

--- js/index.js
+++ js/index.js
@@ -14,32 +14,52 @@ let musicPause = document.querySelector('.music-pause');
 
 // ---------- 一、全局事件 ----------
 
+// ios禁用缩放
+window.onload = function() {
+	document.addEventListener('touchstart', function(event) {
+		if (event.touches.length > 1) {
+			event.preventDefault();
+		}
+	});
+	var lastTouchEnd = 0;
+	document.addEventListener('touchend', function(event) {
+		var now = (new Date()).getTime();
+		if (now - lastTouchEnd <= 300) {
+			event.preventDefault();
+		}
+		lastTouchEnd = now;
+	}, false);
+	document.addEventListener('gesturestart', function(event) {
+		event.preventDefault();
+	});
+}
+
 // 清除浏览器默认滑动行为
-document.ontouchmove = function(){
+document.ontouchmove = function() {
 	event.preventDefault();
 }
 // 进入首页
 navJump(navIndex);
 // 底部导航栏点击切换
-for(let i = 0, max = navItems.length; i < max; i++){
-	navItems[i].addEventListener('click',function(){
+for (let i = 0, max = navItems.length; i < max; i++) {
+	navItems[i].addEventListener('click', function() {
 		navJump(i);
 		// 切换页面时回到游戏进入前状态
 		gameBegin.classList.remove('hide');
-		if(!backgroundMusic.paused){
+		if (!backgroundMusic.paused) {
 			backgroundMusic.pause();
 			backgroundMusicSwitchContent.classList.toggle('active');
 		}
 	});
-	navItems[i].addEventListener('mousedown',function(){
+	navItems[i].addEventListener('mousedown', function() {
 		event.preventDefault();
 	});
 };
 // 音乐播放
 music.volume = .6;
-musicPlay.addEventListener('click',function(){
+musicPlay.addEventListener('click', function() {
 	// 暂停游戏背景音
-	if(!backgroundMusic.paused){
+	if (!backgroundMusic.paused) {
 		backgroundMusic.pause();
 		backgroundMusicSwitchContent.classList.toggle('active');
 	}
@@ -48,18 +68,18 @@ musicPlay.addEventListener('click',function(){
 	musicPause.classList.toggle('hide');
 	music.play();
 });
-musicPause.addEventListener('click',function(){
+musicPause.addEventListener('click', function() {
 	musicPlay.classList.toggle('hide');
 	musicPause.classList.toggle('hide');
 	music.pause();
 });
 // 消息
-headerLeft.addEventListener('click',function(){
+headerLeft.addEventListener('click', function() {
 	messageMask.classList.add('active');
 	messageCount.classList.add('hide');
 })
-messageRead.addEventListener('click',function(){
-	event.cancelBubble=true;
+messageRead.addEventListener('click', function() {
+	event.cancelBubble = true;
 	messageMask.classList.remove('active');
 	messageRead.classList.add('active');
 })
@@ -67,14 +87,14 @@ messageRead.addEventListener('click',function(){
 // ---------- 一、全局函数 ----------
 
 // 底部导航栏点击切换
-function navJump(index){
+function navJump(index) {
 	// 导航栏切换
 	navItems[navIndex].classList.remove('active');
 	navItems[index].classList.add('active');
 	// 主体内容切换
 	mainItems[navIndex].classList.remove('active');
 	mainItems[index].classList.add('active');
-	
+
 	navIndex = index;
 };
 
@@ -93,24 +113,25 @@ photoFullScreen(homepagePhotos);
 
 // 照片全屏查看
 // @param：arr，需要添加全屏查看功能的图片数组
-function photoFullScreen(arr){
-	for(let i = 0, max = arr.length; i < max; i++){
-		arr[i].addEventListener('click',function(){
-			event.cancelBubble=true;
+function photoFullScreen(arr) {
+	for (let i = 0, max = arr.length; i < max; i++) {
+		arr[i].addEventListener('click', function() {
+			event.cancelBubble = true;
 			photoClick(arr[i]);
 		});
 	}
-	function photoClick(img){
-		if(!isFullScreen){
+
+	function photoClick(img) {
+		if (!isFullScreen) {
 			img.parentNode.classList.add('full-screen');
 			img.classList.add('full-screen');
 			isFullScreen = true;
-			img.parentNode.addEventListener('click',function(){
+			img.parentNode.addEventListener('click', function() {
 				img.parentNode.classList.remove('full-screen');
 				img.classList.remove('full-screen');
 				isFullScreen = false;
 			});
-		}else{
+		} else {
 			img.parentNode.classList.remove('full-screen');
 			img.classList.remove('full-screen');
 			isFullScreen = false;
@@ -133,7 +154,7 @@ let originX = workExperienceSwiper.offsetLeft;
 // 显示左右切换按钮
 showArrow();
 // 点击切换工作经历
-turnLeft.addEventListener('click',function(){
+turnLeft.addEventListener('click', function() {
 	stopTransition();
 	workExperienceSwiper.style.transition = '.3s';
 	originX = originX + workExperienceItem[0].offsetWidth;
@@ -141,7 +162,7 @@ turnLeft.addEventListener('click',function(){
 	workExperienceIndex--;
 	showArrow();
 })
-turnRight.addEventListener('click',function(){
+turnRight.addEventListener('click', function() {
 	stopTransition();
 	workExperienceSwiper.style.transition = '.3s';
 	originX = originX - workExperienceItem[0].offsetWidth;
@@ -150,29 +171,30 @@ turnRight.addEventListener('click',function(){
 	showArrow();
 })
 // 滑动切换工作经历
-workExperienceSwiper.addEventListener('touchstart',function(){
+workExperienceSwiper.addEventListener('touchstart', function() {
 	stopTransition();
 	event.preventDefault();
 	let startX = event.touches[0].clientX;
-	document.ontouchmove = function(){
+	document.ontouchmove = function() {
 		let moveX = event.touches[0].clientX;
 		workExperienceSwiper.style.left = originX + moveX - startX + 'px';
 	};
-	document.ontouchend = function(){
+	document.ontouchend = function() {
 		workExperienceSwiper.style.transition = '.3s';
-		if(workExperienceSwiper.offsetLeft <= 0 && workExperienceSwiper.offsetLeft >= -workExperienceSwiper.offsetWidth + workExperienceItem[0].offsetWidth){
-			if(workExperienceSwiper.offsetLeft - originX <= -workExperienceItem[0].offsetWidth/10){
+		if (workExperienceSwiper.offsetLeft <= 0 && workExperienceSwiper.offsetLeft >= -workExperienceSwiper.offsetWidth +
+			workExperienceItem[0].offsetWidth) {
+			if (workExperienceSwiper.offsetLeft - originX <= -workExperienceItem[0].offsetWidth / 10) {
 				originX = originX - workExperienceItem[0].offsetWidth;
 				workExperienceSwiper.style.left = originX + 'px';
 				workExperienceIndex++;
-			}else if(workExperienceSwiper.offsetLeft - originX >= workExperienceItem[0].offsetWidth/10){
+			} else if (workExperienceSwiper.offsetLeft - originX >= workExperienceItem[0].offsetWidth / 10) {
 				originX = originX + workExperienceItem[0].offsetWidth;
 				workExperienceSwiper.style.left = originX + 'px';
 				workExperienceIndex--;
-			}else{
+			} else {
 				workExperienceSwiper.style.left = originX + 'px';
 			}
-		}else{
+		} else {
 			workExperienceSwiper.style.left = originX + 'px';
 		}
 		showArrow();
@@ -185,21 +207,21 @@ workExperienceSwiper.addEventListener('touchstart',function(){
 // ---------- 四、工作经历函数 ----------
 
 // 结束当前切换动画
-function stopTransition(){
+function stopTransition() {
 	workExperienceSwiper.style.transition = '0s';
 	workExperienceSwiper.style.left = originX + 'px';
 }
 // 显示左右切换按钮/页码切换
-function showArrow(){
+function showArrow() {
 	turnLeft.classList.remove('hide');
 	turnRight.classList.remove('hide');
-	if(originX >= 0){
+	if (originX >= 0) {
 		turnLeft.classList.add('hide');
-	}else if(originX <= -workExperienceSwiper.offsetWidth + workExperienceItem[0].offsetWidth){
+	} else if (originX <= -workExperienceSwiper.offsetWidth + workExperienceItem[0].offsetWidth) {
 		turnRight.classList.add('hide');
 	}
 	// 页码切换
-	for(let i = 0, max = pageNumberItem.length; i < max; i++){
+	for (let i = 0, max = pageNumberItem.length; i < max; i++) {
 		pageNumberItem[i].classList.remove('active');
 	}
 	pageNumberItem[workExperienceIndex].classList.add('active');
@@ -210,8 +232,8 @@ let technologyItems = document.querySelectorAll('.technology-item');
 
 // ---------- 五、技术栈事件 ----------
 // 点击翻转
-for(let i = 0, max = technologyItems.length; i < max; i++){
-	technologyItems[i].addEventListener('click',function(){
+for (let i = 0, max = technologyItems.length; i < max; i++) {
+	technologyItems[i].addEventListener('click', function() {
 		technologyItems[i].classList.toggle('active');
 	})
 }
